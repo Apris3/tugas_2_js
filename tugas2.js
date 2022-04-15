42fe4678bffe990aa30360d6bf99289d0b17002c
Apris3 <turaamahapris@gmail.com>
2022-04-16 01:04:31 +0800

    Tugas 2 js
    Tugas ini saya serahkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+var nomor = 10;
+var huruf ='Niomic';
+var salah = false;
+var tidak;
+var arr = [45,'ayu',false,'budi', 60];
+
+
+console.log(nomor);
+console.log(huruf);
+console.log(salah);
+console.log(tidak);
+console.log(arr[1]);
